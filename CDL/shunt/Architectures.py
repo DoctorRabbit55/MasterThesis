6ac824675be8ce5e1c31f30b9367d07def669e58
Haas Bernhard <bha@mission-embedded.com>
2020-07-29 11:28:30 +0200

    Fixed last commit.

--- CDL/shunt/Architectures.py
+++ CDL/shunt/Architectures.py
@@ -37,7 +37,7 @@ def createArch4(input_shape, output_shape):
     x = Conv2D(input_shape[-1], kernel_size=(1,1), strides=(1,1), padding='same', use_bias=False, activation=None, kernel_initializer="he_normal", kernel_regularizer=regularizers.l2(4e-5))(x)
     x = BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
     x = ReLU(6.)(x)
-    x = DepthwiseConv2D(input_shape[-1], kernel_size=(3,3), strides=(1,1), padding='same', use_bias=False, activation=None, kernel_initializer="he_normal", kernel_regularizer=regularizers.l2(4e-5))(x)
+    x = DepthwiseConv2D(input_shape[-1], strides=(1,1), padding='same', use_bias=False, activation=None, kernel_initializer="he_normal", kernel_regularizer=regularizers.l2(4e-5))(x)
     x = BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
     x = ReLU(6.)(x)
     x = Conv2D(output_shape[-1], kernel_size=(1,1), strides=(1,1), padding='same', use_bias=False, activation=None, kernel_initializer="he_normal", kernel_regularizer=regularizers.l2(4e-5))(x)
