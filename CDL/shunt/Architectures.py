dd482a03c8787939b74c068d99f06a453499ab96
Haas Bernhard <bha@mission-embedded.com>
2020-07-29 11:20:56 +0200

    Updated arch4.

--- CDL/shunt/Architectures.py
+++ CDL/shunt/Architectures.py
@@ -34,18 +34,15 @@ def createArch4(input_shape, output_shape):
     input_net = Input(shape=input_shape)
     x = input_net
 
-    kernel_size = (3,3)
-
-    #x = Conv2D(input_shape[-1], kernel_size=kernel_size, strides=(1,1), padding='same', use_bias=False, activation=None, kernel_initializer="he_normal", kernel_regularizer=regularizers.l2(4e-5))(x)
-    #x = BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
-    #x = ReLU(6.)(x)
-    x = Conv2D(input_shape[-1], kernel_size=kernel_size, strides=(1,1), padding='same', use_bias=False, activation=None, kernel_initializer="he_normal", kernel_regularizer=regularizers.l2(4e-5))(x)
+    x = Conv2D(input_shape[-1], kernel_size=(1,1), strides=(1,1), padding='same', use_bias=False, activation=None, kernel_initializer="he_normal", kernel_regularizer=regularizers.l2(4e-5))(x)
     x = BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
     x = ReLU(6.)(x)
-    x = Conv2D(output_shape[-1], kernel_size=kernel_size, strides=(1,1), padding='same', use_bias=False, activation=None, kernel_initializer="he_normal", kernel_regularizer=regularizers.l2(4e-5))(x)
+    x = DepthwiseConv2D(input_shape[-1], kernel_size=(3,3), strides=(1,1), padding='same', use_bias=False, activation=None, kernel_initializer="he_normal", kernel_regularizer=regularizers.l2(4e-5))(x)
     x = BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
-    #x = Add()([input_net, x])
-
+    x = ReLU(6.)(x)
+    x = Conv2D(output_shape[-1], kernel_size=(1,1), strides=(1,1), padding='same', use_bias=False, activation=None, kernel_initializer="he_normal", kernel_regularizer=regularizers.l2(4e-5))(x)
+    x = BatchNormalization(epsilon=1e-3, momentum=0.999)(x)
+    
     model = Model(inputs=input_net, outputs=x, name='shunt')
 
     return model
