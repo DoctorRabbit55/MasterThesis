eb4ea364b2d6ba45f919efd16e262fc38b73d322
Haas Bernhard <bha@mission-embedded.com>
2020-09-09 14:51:49 +0200

    fix

--- main.py
+++ main.py
@@ -306,7 +306,7 @@ if __name__ == '__main__':
             train_dummy_data = np.zeros((len_train_data, batch_size_shunt,))
             datagen_val_dummy = Imagenet_train_shunt_generator(dataset_val_image_path, dataset_ground_truth_file_path, shuffle=False)
 
-            history_shunt = model_training_shunt.fit(zip(datagen_train.flow_from_directory(dataset_train_image_path, class_mode=None, shuffle=True, target_size=(224,224), batch_size=batch_size_shunt), train_dummy_data), epochs=epochs_shunt, steps=len_train_data//batch_size_shunt, validation_data=datagen_val_dummy, verbose=1, callbacks=[callback_checkpoint, callback_learning_rate])
+            history_shunt = model_training_shunt.fit(zip(datagen_train.flow_from_directory(dataset_train_image_path, class_mode=None, shuffle=True, target_size=(224,224), batch_size=batch_size_shunt), train_dummy_data), epochs=epochs_shunt, steps_per_epoch=len_train_data//batch_size_shunt, validation_data=datagen_val_dummy, verbose=1, callbacks=[callback_checkpoint, callback_learning_rate])
 
 
         elif dataset_name == 'CIFAR10':
