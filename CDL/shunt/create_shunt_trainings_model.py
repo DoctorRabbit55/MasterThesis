50a4d2254bfd9695890882c5005063aacbef3e32
Haas Bernhard <bha@mission-embedded.com>
2020-09-10 08:30:26 +0200

    fix

--- CDL/shunt/create_shunt_trainings_model.py
+++ CDL/shunt/create_shunt_trainings_model.py
@@ -2,6 +2,7 @@ import keras
 from keras.utils.generic_utils import get_custom_objects
 from keras.layers import deserialize as layer_from_config
 from keras.layers import Input, Add, Multiply, Subtract, Reshape
+from keras.backend import int_shape
 
 import numpy as np
 
@@ -47,11 +48,11 @@ def create_shunt_trainings_model(model, model_shunt, shunt_locations):
             shunt_input = x
 
     output_original_model = x
-    output_original_model = Reshape((output_original_model.output_shape[1]*output_original_model.output_shape[2]*output_original_model.output_shape[3]))(output_original_model)
+    output_original_model = Reshape((int_shape(output_original_model)[1]*int_shape(output_original_model)[2]*int_shape(output_original_model)[3]))(output_original_model)
     output_original_model = keras.backend.l2_normalize(output_original_model, axis=1)
 
     x = model_shunt(shunt_input)
-    x = Reshape((x.output_shape[1]*x.output_shape[2]*x.output_shape[3]))
+    x = Reshape((int_shape(x)[1]*int_shape(x)[2]*int_shape(x)[3]))
     x = keras.backend.l2_normalize(x, axis=1)
     x = Subtract()([x, output_original_model])
     x = Multiply()([x, x])
