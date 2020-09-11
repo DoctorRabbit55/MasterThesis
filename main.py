cc42a9587f6e603513212154ca657d68ed78e708
Haas Bernhard <bha@mission-embedded.com>
2020-09-11 14:34:11 +0200

    fix

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ from matplotlib import pyplot as plt
 from sklearn.metrics import classification_report
 
 def mean_squared_diff(y_true, y_pred):
-    #y_pred = ops.convert_to_tensor_v2(y_pred)
+    y_pred = ops.convert_to_tensor_v2_with_dispatch(y_pred)
     return keras.backend.mean(keras.backend.square(y_pred), axis=-1)
 
 
